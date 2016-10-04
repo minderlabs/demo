19f5644076febd333c1af84541addc1c5d819041
Trey Lawrence <lawrence.trey@gmail.com>
2016-10-04 18:44:48 -0400

    Initial native commit (#1)
    * Initial native commit
    
    * Redo what iCloud undid
    
    * Fix typo
    
    * Addressing reiview comments

---
+++ sub/NativeApp/index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {AppRegistry} from 'react-native';
+import NativeApp from './common/components/app';
+
+AppRegistry.registerComponent('NativeApp', () => NativeApp);
