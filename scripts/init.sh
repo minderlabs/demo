55be748fe74c9193b001476fba7d4951eb5b9558
Rich Burdon <burdon@burdon-mbp.local>
2017-02-21 13:28:47 -0500

    Command updates.

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,13 @@
 #
 
 brew install jq
+brew install npm
 
 npm install -g firebase-tools
 npm install -g npm-workspace
+npm install -g webpack
+npm install -g karma-cli
+npm install -g grunt
+
+$(dirname -- "$0")/update.sh
+
