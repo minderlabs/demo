71d1b0f65cf402bb623eefce2d349b7cfd9a51d9
Adam Berenzweig <adam.b@gmail.com>
2017-02-24 20:44:01 -0500

    minikube configs

--- sub/jupyter/scripts/build_docker.sh
+++ sub/jupyter/scripts/build_docker.sh
@@ -6,6 +6,8 @@ set -e
 TAG=jupyter
 REPO=jupyter
 VERSION=${1:-latest}
+DOCKER_REPO=${2:-ecr}
+
 NAMESPACE=240980109537.dkr.ecr.us-east-1.amazonaws.com
 
 cd "$(dirname "$0")/.."
@@ -23,13 +25,27 @@ for fname in "${to_copy[@]}"; do
   cp $fname $CONFIG_DIR/
 done
 
+case "$DOCKER_REPO" in
+  minikube)
+    eval $(minikube docker-env)
+    NAMESPACE=minderlabs
+    ;;
+esac
+
 docker build -t ${TAG}:${VERSION} .
 rm -rf $CONFIG_DIR
 
 docker tag ${TAG}:${VERSION} ${NAMESPACE}/${REPO}:${VERSION}
 
-# Login to AWS ECR.
-# https://forums.aws.amazon.com/thread.jspa?messageID=692733
-eval $(AWS_PROFILE=minder aws ecr get-login)
+case "$DOCKER_REPO" in
+  ecr)
+    # Login to AWS ECR.
+    # https://forums.aws.amazon.com/thread.jspa?messageID=692733
+    eval $(AWS_PROFILE=minder aws ecr get-login)
 
-docker push ${NAMESPACE}/${REPO}:${VERSION}
+    docker push ${NAMESPACE}/${REPO}:${VERSION}
+    ;;
+  minikube)
+    echo "Nothing to push for minikube."
+    ;;
+esac
