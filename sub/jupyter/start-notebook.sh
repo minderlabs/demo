9b4f59638e4f03263e1091009630f8aa6ebcb058
Adam Berenzweig <adam.b@gmail.com>
2017-02-26 20:07:49 -0500

    Optionally check out GIT_HASH after cloning.

--- sub/jupyter/start-notebook.sh
+++ sub/jupyter/start-notebook.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-# Run post-deploy install, and then start the notebook.
+# Run post-deploy install, and then start the notebook or run an experiment.
+#
+# Environment parameters:
+#   GIT_HASH: if present, check out this commit hash from the repo.
+#   EXPERIMENT_ID: if present, run this experiment and exit. Otherwise start the notebook.
 
 set -e
 
-cd /notebook
+GIT_REPO="git@github.com:minderlabs/research.git"
+REPO_DIR=research
 
 eval `ssh-agent -s`
 ssh-add $HOME/.ssh/jupyter-keypair.pem
 
+cd /notebook
+
 # Do not clone over existing directory, in case of restarts with persistent volume.
-if [ ! -d /notebook/research ]; then
-  git clone git@github.com:minderlabs/research.git
+if [ ! -d $REPO_DIR ]; then
+  git clone $GIT_REPO
+  if [ ! -z $GIT_HASH ]; then
+    echo Checking out git hash $GIT_HASH
+    pushd $REPO_DIR
+    git checkout $GIT_HASH
+    popd
+  fi
 fi
 
 source activate keras
@@ -22,5 +35,5 @@ pip install -r $HOME/requirements.txt
 if [ -z $EXPERIMENT_ID ]; then
   jupyter notebook --no-browser --ip=0.0.0.0
 else
-  python /notebook/research/scripts/run_training.py --eid $EXPERIMENT_ID
+  python $REPO_DIR/scripts/run_training.py --eid $EXPERIMENT_ID
 fi
